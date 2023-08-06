f94cec9dd179dddb5d0cca8e05168c5383f2fa3f
Berat Akgul <berat.akgul@kariyer.net>
2023-08-07 02:12:49 +0300

    feat: generateStaticParams added

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -3,7 +3,6 @@ import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter'
 
 export default async function Blog({ params }) {
-  console.log('params', params)
   const { content, data} = await getBlogDetail(params.slug);  
 
   return (
@@ -19,8 +18,24 @@ export default async function Blog({ params }) {
 }
 
 export async function getBlogDetail(slug) {
-  console.log('slug', slug)
   const post = matter(fs.readFileSync(`./content/blogs/${slug}.md`, 'utf8'))
-  console.log('blog post', post)
   return  post
+}
+
+export async function generateStaticParams() {
+  const filesInProjects = fs.readdirSync('./content/blogs')
+
+  // Getting the filenames excluding .md extension
+  // and returning an array containing slug (the filename) as params for every route
+  // It looks like this
+  // paths = [
+  //   { params: { slug: 'my-first-blog' }},
+  //   { params: { slug: 'how-to-train-a-dragon' }},
+  //   { params: { slug: 'how-to-catch-a-pokemon' }},
+  // ]
+  const paths = filesInProjects.map(file => {
+    const filename = file.slice(0, file.indexOf('.'))
+    return { params: { slug: filename }}
+  })
+  return paths
 }
